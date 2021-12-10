a1a2cf9683d687c64b169cc0e6e3abbdccd7cf65
Kixel <ElanorUnayar@googlemail.com>
2021-12-10 12:19:21 +0100

    small structure change

--- PFSpellbook/PFSpellbook.py
+++ PFSpellbook/PFSpellbook.py
@@ -17,13 +17,18 @@ bookart = """
                                `---`
 """
 
-if __name__ == "__main__":
-    print("="*100+"\n                  Pathfinder 1E Spellbook Generator\n"+bookart+"\n"+"="*100)
+
+def parseArgs() -> argparse.Namespace:
     ap = argparse.ArgumentParser(prog="Pathfinder Spellbook Generator")
     ap.add_argument("-r", "--rip", action="store", choices=choicesforR, help="Rips data from the specified source before doing anything else. If not specified, will use available data, or exit if nothing is found.")
     ap.add_argument("-s", "--storedata", action="store_true", help="Stores ripped data for later use.")
     ap.add_argument("-c", "--combinerip", action="store_true", help="Will append and replace old data, not completely remove old data and rewrite it")
     
     t = ap.parse_args()
+    return t
+    
 
+if __name__ == "__main__":
+    print("="*100+"\n                  Pathfinder 1E Spellbook Generator\n"+bookart+"\n"+"="*100)
+    arg = parseArgs()    
 
