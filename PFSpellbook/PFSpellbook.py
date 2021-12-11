0bb97f305f5dbe7619d652dcd187e083f367c877
Kixel <elanorunayar@googlemail.com>
2021-12-11 23:41:18 +0100

    Added arguments and env. detection

--- PFSpellbook/PFSpellbook.py
+++ PFSpellbook/PFSpellbook.py
@@ -2,9 +2,12 @@
 
 import argparse
 import jinja2
+import os
 
+indir = "./in"
+outdir = "./out"
 
-
+defaulttemplate = "defaultall"
 
 choicesforR = ["d20pfsrd", "aon", "5footstep"]
 bookart = """
@@ -18,17 +21,39 @@ bookart = """
 """
 
 
+
+def getAvailableTemplates() -> list:
+    avtemp = []
+    
+    
+
 def parseArgs() -> argparse.Namespace:
     ap = argparse.ArgumentParser(prog="Pathfinder Spellbook Generator")
     ap.add_argument("-r", "--rip", action="store", choices=choicesforR, help="Rips data from the specified source before doing anything else. If not specified, will use available data, or exit if nothing is found.")
     ap.add_argument("-s", "--storedata", action="store_true", help="Stores ripped data for later use.")
     ap.add_argument("-c", "--combinerip", action="store_true", help="Will append and replace old data, not completely remove old data and rewrite it")
+    ap.add_argument("-f", "--filter", action="store", help="a csv file to filter the spellbook output")
+    ap.add_argument("-t", "--template", action="store", choices=getAvailableTemplates(), help="the name of the template", default=defaulttemplate)
+    ap.add_argument("--custom-template", action="store", help="Add the path to another compatible html file for a template here")
+    ap.add_argument("-i", "--interactive", action="store_true", help="Starts PFSpells in interactive mode")
+    ap.add_argument("-b", "--build", action="store_true", help="Parse the cached data to an output file")
+    ap.add_argument("-o", "--output", action="store", help="Set the output file name and location", default="./spellbook.pdf")
     
     t = ap.parse_args()
     return t
     
 
+def updateEnv():
+    if os.environ.get("ISDOCKER", False):
+        print("[?] This is a docker environment!")
+        print("[?] Please ensure you have mounted folders to /in and /out")
+    else:
+        print("[?] Running in CLI mode")
+        indir = ""
+        outdir = ""
+
 if __name__ == "__main__":
     print("="*100+"\n                  Pathfinder 1E Spellbook Generator\n"+bookart+"\n"+"="*100)
+    updateEnv()
     arg = parseArgs()    
 
