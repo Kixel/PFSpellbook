69a66dd6080ed0e90780583bd8db69bc05466503
Kixel <ElanorUnayar@googlemail.com>
2023-03-13 07:44:03 +0100

    Added more files for last commit

--- PFSpellbook/loader.py
+++ PFSpellbook/loader.py
@@ -3,12 +3,17 @@
 import requests as req
 import os
 import tools.persistent_naming as pn
+from tools.spell import Spell
 from abc import ABC, abstractmethod
 from typing import Any
 import configparser
 import time
 import random
 import urllib.parse
+from xml.dom import minidom
+import qrcode
+import qrcode.image.svg
+import pprint as pp
 
 
 class AnyLoader(ABC):
@@ -94,9 +99,10 @@ class AON(AnyLoader):
         with open(os.path.join(self._cachebase, self.linkcache), 'r') as linkcachereader:
             lines = linkcachereader.readlines()
             cl_i = 0
-            cl_max = 5000
+            cl_max = 1
             cl_interval = 5
             cl_random = 3
+            unusuals = []
             for cline in lines:
                 if cl_i >= cl_max:
                     break
@@ -105,11 +111,11 @@ class AON(AnyLoader):
                 sname = clineparts[0]
                 fname = pn.spell2filename(sname)
                 dldata = ""
+                flink = clineparts[1].replace("\n", "")
+                flink_clean = urllib.parse.quote(flink, safe=":/?&=")
                 print("[?] #"+str(cl_i)+" Spellname: "+sname)
                 if not os.path.exists(os.path.join(self._cachebase, fname)):
                     os.makedirs(os.path.join(self._cachebase, fname))
-                    flink = clineparts[1].replace("\n", "")
-                    flink_clean = urllib.parse.quote(flink, safe=":/?&=")
                     print("[?] Downloading from "+flink_clean)
                     dldata = req.get(flink_clean).text
                     with open(os.path.join(self._cachebase, fname, "rawdata.html"), 'w') as rawwriter:
@@ -122,9 +128,96 @@ class AON(AnyLoader):
                     with open(os.path.join(self._cachebase, fname, "rawdata.html"), 'r') as rawreader:
                         dldata = rawreader.read()
                         print("[?] Found data in cache folder")
+                        
+                #create qr code if not existing
+                if not os.path.exists(os.path.join(self._cachebase, fname, "qr.svg")):
+                    with open(os.path.join(self._cachebase, fname, "qr.svg"), 'wb') as qr:
+                        qrimg = qrcode.make(flink_clean, image_factory=qrcode.image.svg.SvgImage)
+                        #qrf = qrcode.QRCode(error_correction = qrcode.constants.ERROR_CORRECT_H)
+                        #qrf.add_data(qr)
+                        #aa = qrf.make(fit=True)
+                        qrimg.save(qr)
                 #code to parse that file TODO
-                    
-                
+                dl_bottom = dldata.split('<span id="ctl00_MainContent_DataListTypes_ctl00_LabelName">', 1)[1]
+                dl_data = "<span>" + dl_bottom.split("\n", 1)[0]
+                print(dl_data)
+                ##clear images
+                dl_data = dl_data.replace("<img", "??img")
+                spelldoc = minidom.parseString(dl_data)
+                foundspell = False
+                newspell = Spell()
+                currentfeature = ""
+                for node in spelldoc.childNodes[0].childNodes:
+                    if node.nodeName == "h1":
+                        childdata = node.childNodes[0].data
+                        lastthing = childdata.split('">')[-1].lstrip(" ").rstrip(" ")
+                        if not foundspell:
+                        #seek to current spell in case of multiple spells
+                            if sname == lastthing:
+                                foundspell = True       
+                        else:
+                            if not (sname == lastthing):
+                                foundspell = False
+                    if foundspell:
+                        if node.nodeName == "#text":
+                            if node.data == " " or node.data == ", " or node.data == " (" or node.data == ") [" or node.data == "]; ":
+                                continue
+                        if currentfeature == "description":
+                            newspell.description = newspell.description + node.toxml()
+                            continue
+                        elif currentfeature == "source":
+                            if node.nodeName == "a":
+                                if node.firstChild.nodeName == "i":
+                                    try:
+                                        newspell.source.append(node.firstChild.firstChild.data)
+                                        newspell.source.append(node.firstChild.firstChild.data.split("pg.")[0])
+                                    except:
+                                        unusuals.append(node.toxml())
+                            else:
+                                unusuals.append(node.toxml())
+                        
+                        if node.nodeName == "h1":
+                            newspell.name = sname
+                            newspell.url = flink_clean
+                            if "ThreeFiveSymbol.gif" in node.childNodes[0].data:
+                                newspell.threefive = True
+                            if "PathfinderSocietySymbol.gif" in node.childNodes[0].data:
+                                newspell.pfslegal = True
+                            if "PathfinderSocietySymbolN.gif" in node.childNodes[0].data:
+                                newspell.pfsrestricted = True
+                        if node.nodeName == "b":
+                            bcontent = node.firstChild.data
+                            if bcontent == "Source":
+                                currentfeature = "source"
+                            elif bcontent == "School":
+                                currentfeature = "school"
+                            elif bcontent == "Level":
+                                currentfeature = "level"
+                            elif bcontent == "Casting Time":
+                                currentfeature = "casttime"
+                            elif bcontent == "Components":
+                                currentfeature = "components"
+                            elif bcontent == "Range":
+                                currentfeature = "range"
+                            elif bcontent == "Target":
+                                currentfeature = "target"
+                            elif bcontent == "Duration":
+                                currentfeature = "duration"
+                            elif bcontent == "Saving Throw":
+                                currentfeature = "save"
+                            elif bcontent == "Spell Resistance":
+                                currentfeature = "sr"
+                            elif bcontent == "Description":
+                                currentfeature = "description"
+                        
+                        
+                        
+                        
+                        
+                        
+                pp.pprint(newspell.__dict__)
+                        
+                        
                 
             
             
